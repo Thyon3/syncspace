040eb06c44d3352d95512ce4fafc1eda4a1cb56a
Thyon3 <asnakemengesha80@gmail.com>
2026-02-28 22:20:21 -0500

    add user routes for settings

--- backend/src/routers/user.route.js
+++ backend/src/routers/user.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { updateProfile } from '../controllers/user.controller.js';
+import { updateProfile, changePassword, deleteAccount, toggleBlockUser } from '../controllers/user.controller.js';
 import multer from "multer";
 import { protectRoute } from '../middleware/auth.middleware.js';
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.put("/updateProfile", upload.single("profilePic"), protectRoute, updateProfile);
+router.post("/change-password", protectRoute, changePassword);
+router.delete("/delete-account", protectRoute, deleteAccount);
+router.post("/toggle-block", protectRoute, toggleBlockUser);
 
 export default router; 
